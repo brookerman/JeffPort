520cece2f5b32f2f73ff7e747d7c6924bbf14047
JEFF BROOKER <brookerman17@gmail.com>
2016-02-17 13:52:15 -0800

    added portfolio index and styles

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root 'welcome#jeffBrookerSplash'
+  resources :welcome
 
   # Example of regular route:
-  get 'products/:id' => 'catalog#view'
+  # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
